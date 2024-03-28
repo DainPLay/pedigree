14b3044896c8042492d991245816be1e0dbd38f3
Dain_PLay <dainplay@gmail.com>
2024-03-28 22:40:07 +0300

    Валидация дочерних форм #150

--- vue/src/components/pages/EditPersonPage.vue
+++ vue/src/components/pages/EditPersonPage.vue
@@ -72,6 +72,36 @@ export default {
       'editPerson'
     ]),
     editPersonHandler () {
+      for (let i = 0; i < this.form.militaries.length; i++) {
+        if (this.form.militaries[i].length === 0) {
+          alert('Ошибка. Форма (Военная служба ' + (i + 1) + ') пустая!');
+        return;
+        }
+      }
+      for (let i = 0; i < this.form.weddings.length; i++) {
+        if (this.form.weddings[i].length === 0) {
+          alert('Ошибка. Форма (Свадьба ' + (i + 1) + ') пустая!');
+        return;
+        }
+      }
+      for (let i = 0; i < this.form.educations.length; i++) {
+        if (this.form.educations[i].length === 0) {
+          alert('Ошибка. Форма (Образование ' + (i + 1) + ') пустая!');
+        return;
+        }
+      }
+      for (let i = 0; i < this.form.works.length; i++) {
+        if (this.form.works[i].length === 0) {
+          alert('Ошибка. Форма (Работа ' + (i + 1) + ') пустая!');
+        return;
+        }
+      }
+      for (let i = 0; i < this.form.children.length; i++) {
+        if (this.form.children[i].length === 0) {
+          alert('Ошибка. Форма (Ребёнок ' + (i + 1) + ') пустая!');
+        return;
+        }
+      }
       this.editPerson(this.form)
       this.goBack()
     },
